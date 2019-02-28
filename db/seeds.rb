b32dfafce4b23acc5f9fa7e8a1410cd28a1a14d3
Brandon Tran <brandt2@uci.edu>
2019-02-28 14:09:29 -0800

    Add demo user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+user = User.create({fname: "demo", lname: "demo", username: "demo", email: "demo@demo.com", password: "password"})
