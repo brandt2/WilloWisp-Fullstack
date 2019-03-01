d94ded7872c8813630149fd44270e13d335b1c2f
Brandon Tran <brandt2@uci.edu>
2019-02-28 17:59:50 -0800

    Work on photos controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,15 +1,23 @@
 class Api::PhotosController < ApplicationController
+  before_action :require_login, only: [:create, :update, :destroy]
 
   def index
-    
+    @photos = Photo.all
+    render :index
   end
 
   def show
-    
+    @photo = Photo.find([params[:id]])
+    if @photo
+      render :show
+    else
+      render json: @photo.errors.full_messages, status: 404
+    end
   end
 
   def create
-    
+    @photo = Photo.new(photo_params)
+    @photo.owner_id = current_user.id
   end
 
   def update
@@ -23,7 +31,7 @@ class Api::PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:title, :description, :owner_id)
+    params.require(:photo).permit(:title, :description)
   end
 
 end
