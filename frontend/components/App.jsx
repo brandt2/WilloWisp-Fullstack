726983fdb03709523e498b3c46512d53ab1913a6
Brandon Tran <brandt2@uci.edu>
2019-03-02 21:48:17 -0800

    Add second navbar

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,6 +7,7 @@ import PhotoIndexContainer from './photos/photo_index_container';
 import PhotoShowContainer from './photos/photo_show_container';
 import CreatePhotoFormContainer from './photos/create_photo_form_container';
 import EditPhotoFormContainer from './photos/edit_photo_form_container';
+import SecondNavBar from './second_nav_bar/second_nav_bar_container';
 import Splash from './splash/splash';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
@@ -14,6 +15,7 @@ const App = () => (
   <div>
     <Route path="/" component={NavBarContainer} />
     <Route exact path ="/" component={Splash} />
+    <Route path="/photos" component={SecondNavBar} />
     <AuthRoute exact path="/login" component={LoginFormContainer} />
     <AuthRoute exact path="/signup" component={SignupFormContainer} />
     <Switch>
--- frontend/components/photos/photo_show_container.jsx
+++ frontend/components/photos/photo_show_container.jsx
@@ -3,7 +3,10 @@ import PhotoShow from './photo_show';
 import { fetchPhoto } from '../../actions/photos_actions';
 
 const mapStateToProps = (state, ownProps) => {
+  const currentUser = state.entities.users[state.session.currentUserId];
+  debugger
   return ({
+    currentUser: currentUser,
     photo: state.entities.photos[ownProps.match.params.photoId],
     photoId: ownProps.match.params.photoId
   });
