6d4f829d0dff690d2e09fd8f21c3b363ecfb2f91
Brandon Tran <brandt2@uci.edu>
2019-03-04 10:39:07 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
+require "open-uri"
 
-user = User.create({fname: "demo", lname: "demo", username: "demo", email: "demo@demo.com", password: "password"})
+User.destroy_all
+Photo.destroy_all
+
+user = User.create!({
+  fname: "demo", 
+  lname: "demo", 
+  username: "demo", 
+  email: "demo@demo.com", 
+  password: "password"
+})
+
+photo1 = Photo.new({
+  title: "dullahan",
+  description: "headless horseman",
+  user: user,
+})
+
+photo1.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/dullahan.jpg'), filename: 'dullahan.jpg')
+photo1.save!
