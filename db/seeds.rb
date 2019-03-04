5f3624bfe5033f2449fa7766beda0b9be364c38c
Brandon Tran <brandt2@uci.edu>
2019-03-04 12:17:29 -0800

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,39 @@ photo1 = Photo.new({
 
 photo1.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/dullahan.jpg'), filename: 'dullahan.jpg')
 photo1.save!
+
+photo2 = Photo.new({
+  title: "Cherry Blossoms",
+  description: "Trip to Japan",
+  user: user,
+})
+
+photo2.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/cherry_blossoms.jpg'), filename: 'cherry_blossoms.jpg')
+photo2.save!
+
+photo3 = Photo.new({
+  title: "Eagle",
+  description: "Eagle looking for prey",
+  user: user,
+})
+
+photo3.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/eagle.jpg'), filename: 'eagle.jpg')
+photo3.save!
+
+photo4 = Photo.new({
+  title: "Orange moon",
+  description: "Very pretty moon",
+  user: user,
+})
+
+photo4.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/orange_moon.jpg'), filename: 'orange_moon.jpg')
+photo4.save!
+
+photo5 = Photo.new({
+  title: "Waterfall into pool",
+  description: "The water has a nice color",
+  user: user,
+})
+
+photo5.image.attach(io: open('https://s3-us-west-1.amazonaws.com/will-o-wisp-seeding/pool.jpg'), filename: 'pool.jpg')
+photo5.save!
