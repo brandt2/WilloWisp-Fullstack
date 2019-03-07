e68cdc87643b12a2646ebc46a3f50aa96bf4ce14
Brandon Tran <brandt2@uci.edu>
2019-03-07 12:10:18 -0800

    Add reducers for comments

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import photosReducer from './photos_reducer';
 import albumsReducer from './albums_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   photos: photosReducer,
   albums: albumsReducer,
+  comments: commentsReducer,
 });
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import photosErrorsReducer from './photos_errors_reducer';
 import albumsErrorsReducer from './albums_errors_reducer';
+import commentsErrorsReducer from './albums_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   photos: photosErrorsReducer,
   albums: albumsErrorsReducer,
+  comments: commentsErrorsReducer,
 });
 
 export default errorsReducer;
