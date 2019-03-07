cb09d238d3ea29c1b7789cd249ba0c6bae268257
Brandon Tran <brandt2@uci.edu>
2019-03-07 10:12:02 -0800

    Work on comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,3 +1,29 @@
 class Api::CommentsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.photo_id = params[:photo][:photo_id].to_i
+    if @comment.save
+
+    else
+      
+    end
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :photo_id)
+  end
 
 end
