7a975a283d467e32aae7b757b6d17d16ed3027a0
Brandon Tran <brandt2@uci.edu>
2019-03-07 11:12:58 -0800

    Make json jbuilder for comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,14 +1,32 @@
 class Api::CommentsController < ApplicationController
-  before_action :require_login
+  before_action :require_login, only: [:create, :update, :destroy]
+
+  def show
+    @comment = Comment.find(params[:id])
+    if @comment
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 404
+    end
+  end
+
+  def index
+    @comments = (
+      if params[:photo_id]
+        Comment.find(params[:photo_id])
+      else
+        Comment.all
+      end
+    )
+  end
 
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
-    @comment.photo_id = params[:photo][:photo_id].to_i
     if @comment.save
-
+      render :show
     else
-      
+      render json: @comment.errors.full_messages, status: 422
     end
   end
 
