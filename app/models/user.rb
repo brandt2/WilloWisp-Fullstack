b3c682c0b6347b3beb8543c0c0689497dcc69adf
Brandon Tran <brandt2@uci.edu>
2019-03-08 09:53:58 -0800

    Add uniqueness validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@
 class User < ApplicationRecord
   validates :fname, :lname, :username, :email, :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
+  validates :username, :email, uniqueness: true
   before_validation :ensure_session_token
 
   attr_reader :password
