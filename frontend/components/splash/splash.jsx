d35065f3a8818046750a85f0b6a18cb22673fa44
Brandon Tran <brandt2@uci.edu>
2019-04-02 14:58:13 -0700

    Enhance spsplash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default () => (
   <div className="splash">
+    <div className="slide1"></div>
+    <div className="slide2"></div>
+    <div className="slide3"></div>
+    <div className="slide4"></div>
     <div className="splash-box">
       <h1 className="splash-tagline">Find your inspiration.</h1>
       <br/>
